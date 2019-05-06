80cdcf34f64e0dfd1615174df5d6783d7191aecb
Marco Antonio Pandolfo <marcopandolfo577@gmail.com>
2019-05-06 20:32:29 -0300

    Pequena correção nas Rotas

--- API/src/config/custom-express.js
+++ API/src/config/custom-express.js
@@ -2,11 +2,11 @@ const express = require('express');
 const consign = require('consign');
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
-const rotas = require('../app/routes/routes');
+const routes = require('../app/routes/routes');
 
 //App
 const app = express();
-rotas(app);
+routes(app);
 
 //BodyParser
 app.use(bodyParser.urlencoded({extended: true}));
