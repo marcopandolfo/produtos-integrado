bb5ad3c11c67f89b8b09ebef6477617f24fa898c
Marco Antonio Pandolfo <marcopandolfo577@gmail.com>
2019-05-06 20:38:01 -0300

    ESLint

--- API/src/config/custom-express.js
+++ API/src/config/custom-express.js
@@ -1,20 +1,20 @@
 const express = require('express');
-const consign = require('consign');
+// const consign = require('consign');
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
 const routes = require('../app/routes/routes');
 
-//App
+// App
 const app = express();
 routes(app);
 
-//BodyParser
-app.use(bodyParser.urlencoded({extended: true}));
+// BodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-//ExpressValidator
+// ExpressValidator
 app.use(expressValidator());
 
-//Consign
+// Consign
 
 module.exports = app;
